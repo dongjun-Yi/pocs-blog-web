c7a46e710a207115a2daa5100d75185ecf8201c4
dongjun-Yi <djyi0223@gmail.com>
2024-01-11 14:58:15 +0900

    notice_detail_edit.js 코드 리팩토링

--- js/notice_detail_edit.js
+++ js/notice_detail_edit.js
@@ -1,12 +1,10 @@
-const Url = window.location.href;
-const arr = Url.split('?postId=');
-const id = arr[1];
-const url = `http://34.64.161.55:80/api/posts/${id}`;
+const getPostIdURL = window.location.href.split('?postId=');
+const postId = getPostIdURL[1];
+const REQUEST_URL = `http://34.64.161.55:80/api/posts/${postId}`;
 let sessiontoken = localStorage.getItem('sessionToken');
 let header = new Headers({ 'x-pocs-session-token': sessiontoken });
 
 let user_Id;
-
 let category;
 
 //공지사항 제목, 공지사항 내용 가져오기
@@ -16,17 +14,29 @@ const flexCheckDefault = document.querySelector('#flexCheckDefault');
 
 const REDIRECT_CODE = 302;
 
-function NoticeEditPage() {
-  fetch(url, { headers: header })
-    .then((response) => response.json())
-    .then((data) => {
-      notice_title.value = `${data.data.title}`;
-      notice_content.value = `${data.data.content}`;
-      flexCheckDefault.checked = data.data.onlyMember;
-      user_Id = data.data.writer.userId;
-      category = data.data.category;
-    });
+function renderNoticeEditPage(data) {
+  notice_title.value = `${data.data.title}`;
+  notice_content.value = `${data.data.content}`;
+  flexCheckDefault.checked = data.data.onlyMember;
+  user_Id = data.data.writer.userId;
+  category = data.data.category;
 }
+
+async function fetchNoticeEditPageData() {
+  const response = await fetch(url, { headers: header });
+  const data = await response.await();
+  await renderNoticeEditPage(data);
+}
+
+function isNoticeEditSucess(result) {
+  return result.status === REDIRECT_CODE;
+}
+
+//목록으로 버튼을 누르면 다시 공지사항목록으로 복귀
+function backToList() {
+  window.location.href = '../html/notices.html';
+}
+
 //공지사항 수정하기 버튼 눌렀을때 호출되는 함수
 async function noticeEdit() {
   const sendData = {
@@ -46,20 +56,13 @@ async function noticeEdit() {
     body: JSON.stringify(sendData),
   };
 
-  const response = await fetch(
-    `http://34.64.161.55:80/api/posts/${id}`,
-    options
-  );
+  const response = await fetch(REQUEST_URL, options);
   const result = await response.json();
-  if (result.status === REDIRECT_CODE) {
+  if (isNoticeEditSucess(result)) {
     backToList();
-  } else {
-    console.log(result.message);
+    return;
   }
-}
-//목록으로 버튼을 누르면 다시 공지사항목록으로 복귀
-function backToList() {
-  window.location.href = '../html/notices.html';
+  alert(result.message);
 }
 
-NoticeEditPage();
+fetchNoticeEditPageData();
